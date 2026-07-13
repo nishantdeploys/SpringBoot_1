c76f389c8b57347b13fec745028dc380d698cd58
Nishant Kumar <nishant098097@gmail.com>
2026-07-13 12:42:55 +0530

    Application context Student Class

--- demo/src/main/java/com/nishant/demo/DemoApplication.java
+++ demo/src/main/java/com/nishant/demo/DemoApplication.java
@@ -2,18 +2,36 @@ package com.nishant.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(DemoApplication.class, args);
+//Holding the reference of student classs With the help of ApplicationContext
+
+		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+
+		Student student = context.getBean(Student.class);
+
+		student.setName("Rahul");
+		student.setAge(30);
+
+		System.out.println(student.getName());
+		System.out.println(student.getAge());
+
+/*
+
+	------------Without holding the reference of the object---------------
+
 		Student student = new Student("Nishxt",22);
 
 		System.out.println(student.getName());
 		System.out.println(student.getAge());
 
+
+ */
 	}
 
 }
---
+++ demo/src/main/java/com/nishant/demo/Test.java
@@ -0,0 +1,15 @@
+package com.nishant.demo;
+
+class P{
+    void m1(){}
+    void m2(){}
+
+}class c1 extends P{
+    void m3(){
+
+    }
+}
+
+public class Test {
+
+}
