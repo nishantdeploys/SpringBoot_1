a1125ebb529fa6662685898827e0e43694f57752
Nishant Kumar <nishant098097@gmail.com>
2026-07-13 11:11:07 +0530

    First Commit

--- demo/src/main/java/com/nishant/demo/DemoApplication.java
+++ demo/src/main/java/com/nishant/demo/DemoApplication.java
@@ -7,7 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
+		Student student = new Student("Nishxt",22);
+
+		System.out.println(student.getName());
+		System.out.println(student.getAge());
+
 	}
 
 }
