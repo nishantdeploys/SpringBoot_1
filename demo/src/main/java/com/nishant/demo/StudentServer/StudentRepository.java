3e5f1707a22e5e2d2c79605ac06a54da153057e2
Nishant Kumar <nishant098097@gmail.com>
2026-07-15 11:28:30 +0530

    Data JPA

--- demo/src/main/java/com/nishant/demo/StudentServer/StudentRepository.java
+++ demo/src/main/java/com/nishant/demo/StudentServer/StudentRepository.java
@@ -1,15 +1,18 @@
 package com.nishant.demo.StudentServer;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class StudentRepository {
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 
-    public Student save(Student student){
-        System.out.println("Student Info Saved");
-        return student;
-    }
-    
+
+//    public Student save(Student student){
+//        System.out.println("Student Info Saved");
+//        return student;
+//    }
+
+
 }
